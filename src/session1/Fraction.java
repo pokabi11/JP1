8150a839e99c4ec0e47ea6716a049a209c19620c
pokabi11 <45455676+pokabi11@users.noreply.github.com>
2022-10-13 06:32:21 +0700

    test commit

--- src/session1/Fraction.java
+++ src/session1/Fraction.java
@@ -1,5 +1,46 @@
 package session1;
 
 public class Fraction {
+    int tuso, mauso;
+
+    public int getTuso() {
+        return tuso;
+    }
+
+    public void setTuso(int tuso) {
+        this.tuso = tuso;
+    }
+
+    public int getMauso() {
+        return mauso;
+    }
+
+    public void setMauso(int mauso) {
+        this.mauso = mauso;
+    }
+
+    public void inPhanSo(){
+        System.out.println("Phân số"+tuso+mauso);
+    }
+    public int timUSCLN(int a, int b) {
+        while (a != b) {
+            if (a > b) {
+                a -= b;
+            } else {
+                b -= a;
+            }
+        }
+        return a;
+    }
+    public void rutGonPhanSo() {
+        int i = timUSCLN(this.getTuso(), this.getMauso());
+        this.setTuso(this.getTuso() / i);
+        this.setMauso(this.getMauso() / i);
+    }
+
+    public void nghichDaoPhanSo(){
+        int tuSoMoi=this.mauso;
+        int mauSoMoi=this.tuso;
+    }
 
 }
